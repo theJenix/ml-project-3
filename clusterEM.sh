a46eb35537ecc697470d8d06b03596d93b1b0694
theJenix <jesse.rosalia@gatech.edu>
2013-03-31 03:00:36 +0000

    Scripts

--- clusterEM.sh
+++ clusterEM.sh
@@ -1,1 +1,2 @@
-java -cp ../weka-3-6-9/weka.jar weka.filters.unsupervised.attribute.AddCluster -W "weka.clusterers.EM -I $3 -N $2 -M 1.0E-6 -S 100" -I last -i $1 -o $1_outEM.arff
+weka_dir=../weka-3-6-9
+java -cp ../weka-3-6-9/weka.jar weka.filters.unsupervised.attribute.AddCluster -W "weka.clusterers.EM -I $3 -N $2 -M 1.0E-6 -S 100" -I last -i $weka_dir/data/$1 -o $1_outEM_$3.arff
--- clusterKMeans.sh
+++ clusterKMeans.sh
@@ -1,1 +1,2 @@
-java -cp ../weka-3-6-9/weka.jar weka.filters.unsupervised.attribute.AddCluster -W "weka.clusterers.SimpleKMeans -N $2 -S 42" -I last -i $1 -o $1_out.arff
+weka_dir=../weka-3-6-9
+java -cp ../weka-3-6-9/weka.jar weka.filters.unsupervised.attribute.AddCluster -W "weka.clusterers.SimpleKMeans -N $2 -S 42 -I $3" -I last -i $weka_dir/data/$1 -o $1_out_$3.arff
